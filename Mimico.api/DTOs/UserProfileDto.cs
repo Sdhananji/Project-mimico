5b3406b7e432ebc92054a86269d0ee2ab7837060
Supun Dhananji <ksupundhananji@gmail.com>
2025-12-20 22:41:19 +0530

    Started working on user authorization part. fetch user details from the token and authorize the user. for that developed UserProfileDto

---
+++ Mimico.api/DTOs/UserProfileDto.cs
@@ -0,0 +1,12 @@
+namespace Mimico.Api.DTOs
+{
+    public class UserProfileDto
+    {
+        public int Id{get; set;}
+        public string FullName{get; set;}
+
+        public string Email{get; set;}
+
+        public string Role{get; set;}
+    }
+}
