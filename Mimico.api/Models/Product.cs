8f874cc77fbbe815bca3cb00a7a7e6370bff5f40
Supun Dhananji <ksupundhananji@gmail.com>
2025-12-23 20:14:44 +0530

    created the model for adding product images

--- Mimico.api/Models/Product.cs
+++ Mimico.api/Models/Product.cs
@@ -1,9 +1,9 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Mimico.api.Products
+namespace Mimico.api.Models
 {
-    public class Products
+    public class Product
     {
         [Key]
         public int Id{get; set;}
---
+++ Mimico.api/Models/ProductImage.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mimico.api.Models
+{
+    public class ProductImage
+    {
+        [Key]
+        public int Id {get; set;}
+
+        [Required]
+        public int ProductId {get; set;}
+
+       [Required]
+        public string ImagePath {get; set;} = string.Empty;
+
+        public bool IsPrimary {get; set;} = false;
+
+        [ForeignKey(nameof(ProductId))]
+        public Product product {get; set;}
+    }
+}
