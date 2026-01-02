564ecd2a0a61d1ee72e7a3b58865f9d4841d79d9
Supun Dhananji <ksupundhananji@gmail.com>
2026-01-02 22:24:16 +0530

    connect the logout in admin part

--- Mimico.api/mimico-frontend/src/components/AdminSidebar.jsx
+++ Mimico.api/mimico-frontend/src/components/AdminSidebar.jsx
@@ -2,13 +2,16 @@ import { NavLink } from "react-router-dom";
 import "../styles/adminSidebar.css";
 
 function AdminSidebar() {
+  //const navigate = useNavigate();
+  
   return (
     <aside className="admin-sidebar">
-      <h2 className="brand">Mimico Admin</h2>
+      <h2 className="brand">Ishuku ....</h2>
 
       <nav>
         <NavLink to="/admin">Dashboard</NavLink>
         <NavLink to="/admin/products/add">Add Product</NavLink>
+        <NavLink to= "/admin/products/orders">Orders</NavLink>
       </nav>
     </aside>
   );
--- Mimico.api/mimico-frontend/src/components/AdminTopbar.jsx
+++ Mimico.api/mimico-frontend/src/components/AdminTopbar.jsx
@@ -1,10 +1,19 @@
 import "../styles/adminTopbar.css";
+import {useNavigate} from "react-router-dom";
 
 function AdminTopbar() {
+
+  const navigate = useNavigate();
+  const handleLogout = () =>{
+    localStorage.removeItem("token");
+    navigate("/login");
+  }
   return (
     <header className="admin-topbar">
       <h3>Admin Panel</h3>
-      <button>Logout</button>
+      <button className = "logout-btn" onClick={handleLogout}>
+        Logout
+      </button>
     </header>
   );
 }
